94f6767229ae1b60a5a6b93119904bd71c659538
grenzenloseSchublade <hans.mueller@rub.de>
2026-01-31 23:10:49 +0000

    Mini fix

--- src/components/NuggetEntryForm.tsx
+++ src/components/NuggetEntryForm.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { nuggetEntrySchema, type NuggetEntryFormData } from "@/lib/validations";
 import Input from "@/components/ui/Input";
